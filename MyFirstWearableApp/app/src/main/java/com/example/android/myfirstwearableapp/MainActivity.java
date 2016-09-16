ec974907b1a691af2321f266bf980353dbafa19a
Kim Mok <wan.kim.mok@teamaol.com>
2016-09-16 16:15:54 -0400

    included a timer and timertask when button is clicked

--- MyFirstWearableApp/app/src/main/java/com/example/android/myfirstwearableapp/MainActivity.java
+++ MyFirstWearableApp/app/src/main/java/com/example/android/myfirstwearableapp/MainActivity.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends Activity {
 
     private TextView mTextView;
@@ -30,5 +33,20 @@ public class MainActivity extends Activity {
         TextView textView = (TextView) findViewById(R.id.text);
         textView.setText("I've been clicked!");
 
+        //create a timer
+        Timer timer = new Timer();
+        //create a new timer task
+        myTask testFunc = new myTask();
+
+        //schedule timer to perform a task after 10 seconds
+        timer.schedule(testFunc, 10000);
+
+    }
+
+    class myTask extends TimerTask{
+        public void run(){
+            //prints to console
+            Log.i("it's been", "10 seconds");
+        }
     }
 }
